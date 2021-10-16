1739b581a5c77546f10ecd711c0afec8315f10c9
evgeny <ogeyou@mail.ru>
2021-10-16 22:11:21 +0500

    Добавляет в базу данных, плюс к паролю добавляет соль

--- storage/post.go
+++ storage/post.go
@@ -3,10 +3,33 @@ package storage
 import (
 	"context"
 	"fmt"
+	"math/rand"
 
-	"log"
-	"github.com/ogeyou/go-autth.git/storage/psql"
 	"github.com/ogeyou/go-autth.git/model"
+	"github.com/ogeyou/go-autth.git/storage/psql"
+	"golang.org/x/crypto/argon2"
+)
+// Добавляем соль к пароль
+// На время иссследований
+func hashPass(plainPassword, salt string) []byte {
+	hashedPass := argon2.IDKey([]byte(plainPassword), []byte(salt), 1, 64*1024, 4, 32)
+	res := []byte(salt)
+	return append(res, hashedPass...)
+}
+// Добавляем соль к пароль
+// На время иссследований
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+// Добавляем соль к пароль
+// На время иссследований
+var (
+	sizes       = []uint{80, 160, 320}
+	letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 )
 
 func UserCreated(user model.User) int64 {
@@ -17,12 +40,20 @@ func UserCreated(user model.User) int64 {
 
 	var id int64
 
-	_,err := dbpool.Exec(ctx, "INSERT INTO users (login, email, password) VALUES($1, $2, $3)", user.Login, user.Email, user.Password)
+	salt := RandStringRunes(8)
+	
+	pass := hashPass(user.Password, salt)
+
+	const sql = "INSERT INTO users (login, email, password) VALUES($1, $2, $3)"
+
+	_, err := dbpool.Exec(ctx, sql, user.Login, user.Email, pass)
 
 	if err != nil {
-		log.Fatalf("Ошибка при добавлении нового пользователя. %v", err)
+		fmt.Println(err)
 	}
+	
 	fmt.Printf("Новый пользователь успешно прошёл регистрацию %v\n", id)
 
 	return id
 }
+
