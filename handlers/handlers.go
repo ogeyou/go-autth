a23e691a0b2f794061dc771ba4ef04539002dbaf
evgeny <ogeyou@mail.ru>
2021-10-18 00:16:45 +0500

    Что то с логином

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -12,11 +12,13 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/ogeyou/go-autth.git/model"
 	"github.com/ogeyou/go-autth.git/storage"
 	"github.com/ogeyou/go-autth.git/storage/psql"
 )
+
 type Session struct {
 	UserID uint32
 	ID     string
@@ -26,9 +28,83 @@ type ctxKey int
 const sessionKey ctxKey = 1
 
 var (
+	noAuthUrls = map[string]struct{}{
+		"/user/login":  struct{}{},
+		"/user/logout": struct{}{},
+		"/user/reg":    struct{}{},
+		"/courses":    struct{}{},
+		"/":            struct{}{},
+	}
+)
+
+var (
 	ErrNoAuth = errors.New("No session found")
 )
 
+func AuthMiddleware(dbpool *pgxpool.Pool, next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if _, ok := noAuthUrls[r.URL.Path]; ok {
+			next.ServeHTTP(w, r)
+			return
+		}
+		sess, err := CheckSession(dbpool, r)
+		if err != nil {
+			http.Error(w, "Вы не авторизованы", http.StatusUnauthorized)
+			return
+		}
+		ctx := context.WithValue(r.Context(), sessionKey, sess)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+func CheckSession(dbpool *pgxpool.Pool, r *http.Request) (*Session, error) {
+	ctx := context.Background()
+
+
+	sessionCookie, err := r.Cookie("session_id")
+	if err == http.ErrNoCookie {
+		log.Println("CheckSession no cookie")
+		return nil, ErrNoAuth
+	}
+
+	sess := &Session{}
+	row := dbpool.QueryRow(ctx, `SELECT user_id FROM sessions WHERE id = $1;`, sessionCookie.Value)
+	if row != nil {
+		fmt.Println(row)
+	}
+	err = row.Scan(&sess.UserID)
+	if err == pgx.ErrNoRows {
+		log.Println("CheckSession no rows")
+		return nil, ErrNoAuth
+	} else if err != nil {
+		log.Println("CheckSession err:", err)
+		return nil, err
+	}
+
+	sess.ID = sessionCookie.Value
+	return sess, nil
+}
+
+func CreateSession(w http.ResponseWriter, r *http.Request, dbpol *pgxpool.Pool, UserID uint32) error {
+	ctx := context.Background()
+	dbpool := psql.Connect(ctx)
+	defer dbpool.Close()
+	fmt.Println("Смотри, значение передается или нет куки", UserID)
+	sessID := storage.RandStringRunes(32)
+	_, err := dbpool.Exec(ctx, "insert into sessions(id, user_id) VALUES($1, $2);", sessID, UserID)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	cookie := &http.Cookie{
+		Name:    "session_id",
+		Value:   sessID,
+		Expires: time.Now().Add(90 * 24 * time.Hour),
+		Path:    "/",
+	}
+	http.SetCookie(w, cookie)
+	return nil
+}
 func SessionFromContext(ctx context.Context) (*Session, error) {
 	sess, ok := ctx.Value(sessionKey).(*Session)
 	if !ok {
@@ -36,13 +112,57 @@ func SessionFromContext(ctx context.Context) (*Session, error) {
 	}
 	return sess, nil
 }
+
+func DestroySession(w http.ResponseWriter, r *http.Request, dbpol *pgxpool.Pool) error {
+	ctx := context.Background()
+	dbpool := psql.Connect(ctx)
+	defer dbpool.Close()
+	sess, err := SessionFromContext(r.Context())
+	if err == nil {
+		dbpool.Exec(ctx, "DELETE FROM sessions WHERE user_id = $1", sess.ID)
+		fmt.Println("Данные куки удалены из базы данных")
+	}
+	cookie := http.Cookie{
+		Name:    "session_id",
+		Expires: time.Now().AddDate(0, 0, -1),
+		Path:    "/",
+	}
+	http.SetCookie(w, &cookie)
+	return nil
+}
+
 func Index(w http.ResponseWriter, r *http.Request) {
 	_, err := SessionFromContext(r.Context())
 	if err != nil {
 		http.Redirect(w, r, "/user/login", http.StatusFound)
 		return
 	}
-	http.Redirect(w, r, "/courses", http.StatusFound)
+	http.Redirect(w, r, "/courses/", http.StatusFound)
+}
+
+func Courses(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Поздравляем, Вы зарегестрированы!")
+}
+
+func UserLogout(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	dbpool := psql.Connect(ctx)
+	defer dbpool.Close()
+	DestroySession(w, r, dbpool)
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func UserLogin(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	dbpool := psql.Connect(ctx)
+	defer dbpool.Close()
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	fmt.Fprintf(w, "This Login page")
+	w.WriteHeader(http.StatusOK)
+
+	DestroySession(w, r, dbpool)
+	http.Redirect(w, r, "/user/login", http.StatusFound)
 }
 
 func UserRegistration(w http.ResponseWriter, r *http.Request) {
@@ -66,37 +186,3 @@ func UserRegistration(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/courses", http.StatusFound)
 
 }
-
-func CreateSession(w http.ResponseWriter, r *http.Request, dbpol *pgxpool.Pool, UserID uint32) error {
-	ctx := context.Background()
-	dbpool := psql.Connect(ctx)
-	defer dbpool.Close()
-	fmt.Println("Смотри, значение передается или нет куки", UserID)
-	sessID := storage.RandStringRunes(32)
-	_, err := dbpool.Exec(ctx, "insert into sessions(id, user_id) VALUES($1, $2);", sessID, UserID)
-	if err != nil{
-		fmt.Println(err)		
-	}
-
-	cookie := &http.Cookie{
-		Name:    "session_id",
-		Value:   sessID,
-		Expires: time.Now().Add(90 * 24 * time.Hour),
-		Path:    "/",
-	}
-	http.SetCookie(w, cookie)
-	return nil
-}
-
-func UserLogin(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	fmt.Fprintf(w, "This Login page")
-	w.WriteHeader(http.StatusOK)
-}
-
-func Courses(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	fmt.Fprintf(w, "Поздравляем, Вы зарегестрированы!")
-	w.WriteHeader(http.StatusOK)
-}	
-
--- main.go
+++ main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"net/http"
+
 	"github.com/ogeyou/go-autth.git/handlers"
-	"github.com/gorilla/mux"
+	"github.com/ogeyou/go-autth.git/storage/psql"
 )
 
 func main() {
+	// Соединение с экземпляром Postgres
+	ctx := context.Background()
+	dbpool := psql.Connect(ctx)
+	defer dbpool.Close()
+	router := http.NewServeMux()
 
-	router := mux.NewRouter()
-	
 	fmt.Println("Start server ...")
+	router.HandleFunc("/courses/", handlers.Courses)
 
-	router.HandleFunc("/api/registration", handlers.UserRegistration)
-	router.HandleFunc("/courses", handlers.Courses)
+	router.HandleFunc("/user/reg", handlers.UserRegistration)
+	router.HandleFunc("/user/login", handlers.UserLogin)
+	router.HandleFunc("/user/logout", handlers.UserLogout)
+
 	router.HandleFunc("/", handlers.Index)
-	// router.HandleFunc("/api/login", handlers.UserLogin)
+	http.Handle("/", handlers.AuthMiddleware(dbpool, router))
 
-	http.ListenAndServe(":8081", router)
+	http.ListenAndServe(":8081", nil)
 }
